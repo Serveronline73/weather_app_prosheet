4bdae5616c17ad442c4314e7b779092a473bf6cc
dennis <info.dennis.durmus@gmail.com>
2024-09-27 10:43:18 +0200

    more city weather data

--- lib/weather_repository.dart
+++ lib/weather_repository.dart
@@ -1,8 +1,37 @@
+import 'dart:math';
+
 import 'package:weather_app_prosheet/weather_data.dart';
 
 class WeatherRepository {
-  final WeatherData _cityData = WeatherData(
-      city: "Bochum", temperature: 25.5, weatherCondition: "Sonnig");
-
-  WeatherData getWeather() => _cityData;
+  final List<WeatherData> _weatherData = [
+    WeatherData(city: "Dormund", temperature: 22, weatherCondition: "Bewölkt"),
+    WeatherData(city: "Essen", temperature: 19, weatherCondition: "Bewölkt"),
+    WeatherData(city: "Köln", temperature: 23, weatherCondition: "Sonnig"),
+    WeatherData(city: "Hannover", temperature: 22, weatherCondition: "Bewölkt"),
+    WeatherData(
+        city: "Düsseldorf", temperature: 25, weatherCondition: "Sonnig"),
+    WeatherData(city: "Bochum", temperature: 25.5, weatherCondition: "Sonnig"),
+  ];
+  WeatherData getWeather() {
+    final random = Random();
+    return _weatherData[random.nextInt(_weatherData.length)];
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//class WeatherRepository {
+  //final WeatherData _cityData = WeatherData(
+    //  city: "Bochum", temperature: 25.5, weatherCondition: "Sonnig");
+
+  //WeatherData getWeather() => _cityData;
+//}
